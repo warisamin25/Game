b031af68497f985473c0e991734c3c5d61d7bfa5
warisamin25 <waris.amin@trantorinc.com>
2020-06-25 23:05:50 +0530

    Update Online.py

--- Online.py
+++ Online.py
@@ -10,15 +10,16 @@ def Start():
     
 @app.route('/user_inputs',  methods=[ 'GET', 'POST'])
 def inputs():
-    #import pdb;pdb.set_trace()
     results = {}
     if request.method == 'POST':
+        # import pdb;pdb.set_trace()
         mood = request.form['input1']
         games = request.form['input2']
-        from Games import main
         set_mood = mood
         set_games = games
-        Games.main()
+        from Games import main
+        games_result, mood = main(set_mood, set_games)
+        print(games_result, mood)
         return render_template('output.html', methods = ['POST'])
     return render_template('User_inputs.html', results=results)
  
